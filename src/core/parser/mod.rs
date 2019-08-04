be604536258780c4efd2c7a2deab8d5272a4a6b7
张世阳 <ZHANGSHIYANG162@pingan.com.cn>
2019-08-04 10:52:52 +0800

    master:[update]

--- src/core/parser/mod.rs
+++ src/core/parser/mod.rs
@@ -6,12 +6,20 @@ use super::utils::expect::{ExpectList, ExpectType};
 use node::NodeType::*;
 pub use node::{Node, NodePosition, NodeType, NodeVal};
 
-pub struct Parser {}
+pub struct Parser {
+    scope_tree: Mutex<Vec<Node>>,
+    expect_list: ExpectList<NodeType, Node>,
+}
 
 impl Parser {
     pub fn parse(tokens: Vec<Token>) {
-        let scope_tree: Mutex<Vec<Node>> = Mutex::new(Vec::new());
-        let mut expect_list:ExpectList<NodeType, Node> = ExpectList::new();
+        let mut parser = Parser {
+            scope_tree: Mutex::new(Vec::new()),
+            expect_list: ExpectList::new(),
+        };
+        parser.parsing(tokens)
+    }
+    pub fn parsing(&mut self, tokens: Vec<Token>) {
         for token in tokens {
             let Token { data, pos } = token;
 
@@ -20,45 +28,47 @@ impl Parser {
                 TokenData::Keyword(keyword) => match keyword {
                     Keyword::Let => {
                         let mut cur_node = Node::new(NodeType::Define, NodePosition::new(pos.x, pos.y));
-                        let mut ref_scope_tree = scope_tree.lock().unwrap();
+                        let mut ref_scope_tree = self.scope_tree.lock().unwrap();
                         Parser::last_scope_add_child(&mut *ref_scope_tree, cur_node);
-                        expect_list.add(NodeType::Variable, ExpectType::After, Box::new(|ident| {
-                            let val = &cur_node.get_val();
+                        let expect_fn = |ident| {
+                            let val = cur_node.get_val();
                             match val {
                                 NodeVal::Define(define) => {
                                     define.set_right(ident);
                                 }
                                 _ => {}
                             }
-                        }));
+                        };
+
+                        self.expect_list.add(NodeType::Variable, ExpectType::After, Box::new(expect_fn));
                     },
                     _ => {}
                 },
                 TokenData::Identifier(_ident) => {
                     let cur_node = Node::new(NodeType::Variable, NodePosition::new(pos.x, pos.y));
-                    expect_list.is_match(&NodeType::Variable, &ExpectType::After, cur_node);
+                    self.expect_list.is_match(&NodeType::Variable, &ExpectType::After, cur_node);
                 }
                 TokenData::Punc(punc) => match punc {
                     Punc::Assign => {
                         let cur_node = Node::new(NodeType::Assign, NodePosition::new(pos.x, pos.y));
-                        let mut ref_scope_tree = scope_tree.lock().unwrap();
+                        let mut ref_scope_tree = self.scope_tree.lock().unwrap();
                         ref_scope_tree.push(cur_node);
                     }
                     Punc::OpenBlock => {
                         let cur_node = Node::new(NodeType::Scope, NodePosition::new(pos.x, pos.y));
-                        let mut ref_scope_tree = scope_tree.lock().unwrap();
+                        let mut ref_scope_tree = self.scope_tree.lock().unwrap();
                         ref_scope_tree.push(cur_node);
 
-                        expect_list.add(NodeType::Scope, ExpectType::After, Box::new(|child_scope| {
-                            let mut ref_scope_tree = &scope_tree.lock().unwrap();
+                        self.expect_list.add(NodeType::Scope, ExpectType::After, Box::new(|child_scope| {
+                            let mut ref_scope_tree = self.scope_tree.lock().unwrap();
                             Parser::last_scope_add_child(&mut ref_scope_tree, child_scope);
                         }));
                     }
                     Punc::CloseBlock => {
-                        let mut ref_scope_tree = scope_tree.lock().unwrap();
+                        let mut ref_scope_tree = self.scope_tree.lock().unwrap();
                         let last_index = ref_scope_tree.len() - 1;
                         let cur_scope = ref_scope_tree.remove(last_index);
-                        expect_list.is_match(&NodeType::Scope, &ExpectType::After, cur_scope);
+                        self.expect_list.is_match(&NodeType::Scope, &ExpectType::After, cur_scope);
                     }
                     _ => {}
                 },
@@ -66,7 +76,7 @@ impl Parser {
                 TokenData::EOF => {}
             }
 
-            println!("{:?}", scope_tree);
+            println!("{:?}", self.scope_tree);
         }
     }
     fn last_scope_add_child(scope_tree: &mut Vec<Node>, child: Node) {
