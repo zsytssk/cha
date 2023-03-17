67d8309d313f8e1edafe7726272fcce5a4161232
shiyangzhang <shiyangzhang@whale.im>
2023-03-17 09:46:47 +0800

    [master]

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,11 @@
-use std::fs::read_to_string;
-mod core;
-
-use crate::core::{executer::Executer, lexer::Lexer, parser::Parser};
-
 fn main() {
-    let source = read_to_string("src/spec/demo.cha").unwrap();
-    let tokens = Lexer::parse(&source);
-    Parser::parse(tokens);
+   let mut a = 1;
+   change(&mut a);
+
+   println!("{}", a);
+}
+
+fn change(a: &mut i32) {
+    println!("{:p}", a);
+    *a = 2;
 }
